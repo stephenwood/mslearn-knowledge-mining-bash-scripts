6ecfe6285a679460dd3c1f962283281ccced02b2
Stephen Wood <stephenwo@outlook.com>
2025-04-25 17:11:31 -0700

    This is the initial commit of the *totally untested* bash scripts for the MSLEARN-KNOWLEDGE-MINING solution. It contains a bash script that corresponds, both in name and heirarchical location, to the cmd files in the original solution.

---
+++ Labfiles/01-azure-search/modify-search/modify-search.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set values for your Search service
+
+url=YOUR_SEARCH_URL
+admin_key=YOUR_ADMIN_KEY
+
+echo -----
+echo Updating the skillset...
+curl -X PUT $url/skillsets/margies-skillset?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: ${admin_key}" -d @skillset.json
+
+# wait
+timeout /t 2 /nobreak
+
+echo -----
+echo Updating the index...
+curl -X PUT $url/indexes/margies-index?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: ${admin_key}" -d @index.json
+
+# wait
+timeout /t 2 /nobreak
+
+echo -----
+echo Updating the indexer...
+curl -X PUT $url/indexers/margies-indexer?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: ${admin_key}" -d @indexer.json
+
+echo -----
+echo Resetting the indexer
+curl -X POST $url/indexers/margies-indexer/reset?api-version=2020-06-30 -H "Content-Type: application/json" -H "Content-Length: 0" -H "api-key: ${admin_key}" 
+
+# wait
+timeout /t 5 /nobreak
+
+echo -----
+echo Rerunning the indexer
+curl -X POST $url/indexers/margies-indexer/run?api-version=2020-06-30 -H "Content-Type: application/json" -H "Content-Length: 0" -H "api-key: ${admin_key}" 
+
---
+++ Labfiles/03-knowledge-store/setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Set values for your subscription and resource group
+
+subscription_id=YOUR_SUBSCRIPTION_ID
+resource_group=YOUR_RESOURCE_GROUP
+location=YOUR_LOCATION_NAME
+
+# Generate the semi-random suffix for the account-names we'll use for the storage account, cognitive services, and search services.
+
+unique_id=$RANDOM$RANDOM
+
+echo "Creating storage...."
+
+storage_account_name="ai102str$unique_id"
+az storage account create --name $storage_account_name --subscription $subscription_id --resource-group $resource_group --location $location --sku Standard_LRS --encryption-services blob --default-action Allow --allow-blob-public-access true --output none
+
+echo "Uploading files...."
+
+# Get the storage account key. NOTE: You must have jq installed on your system to run this command. 
+# To install jq on a Mac on which you have installed homebrew, open a terminal and type: homebrew install jq
+
+AZURE_STORAGE_KEY=$(az storage account keys list --subscription $subscription_id --resource-group $resource_group --account-name $storage_account_name | jq -r '.[0].value')
+az storage container create --account-name $storage_account_name --name margies --public-access blob --auth-mode key --account-key $AZURE_STORAGE_KEY --output none
+az storage blob upload-batch -d margies -s data --account-name $storage_account_name --auth-mode key --account-key $AZURE_STORAGE_KEY  --output none
+
+ai_cognitivie_services_name="ai102cog$unique_id"
+
+echo "Creating Azure AI service account...."
+az cognitiveservices account create --kind CognitiveServices --location $location --name $ai_cognitivie_services_name --sku S0 --subscription $subscription_id --resource-group $resource_group --yes --output none
+
+search_service_name=ai102srch$unique_id
+
+echo "Creating search service...."
+echo "(If this gets stuck at '- Running ..' for more than a minute, press CTRL+C then select N)"
+az search service create --name $search_service_name --subscription $subscription_id --resource-group $resource_group --location $location --sku basic --output none
+
+echo "-------------------------------------"
+echo "Storage account: ${storage_account_name}."
+az storage account show-connection-string --subscription $subscription_id --resource-group $resource_group --name $storage_account_name
+echo "----"
+echo "Azure AI Services account: ${ai_services_account_name}."
+az cognitiveservices account keys list --subscription $subscription_id --resource-group $resource_group --name $ai_cognitivie_services_name
+echo ----
+echo Search Service: ai102srch
+echo  Url: https://${search_service_name}.search.windows.net
+echo  Admin Keys:
+az search admin-key show --subscription $subscription_id --resource-group $resource_group --service-name $search_service_name
+echo  Query Keys:
+az search query-key list --subscription $subscription_id --resource-group $resource_group --service-name $search_service_name
